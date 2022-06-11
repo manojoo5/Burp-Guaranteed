c8e6df85ccb48aec4bf8e957d7c6d79528141361
Manoj Roy <manojkumarv085@gmail.com>
2022-06-11 23:53:08 +0530

    Init with str

---
+++ src/Layouts/Footer/Footer.js
@@ -0,0 +1,30 @@
+import './Footer.css'
+
+// Assests 
+import Location from '../../Assets/location_icon.svg'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="location">
+                <label>Location</label>
+                <div className='location_text_icon'>
+                    <img src={Location} alt="loacation" />
+                    <p>Road No:12,Padma Nagar,Banjara Hills,Hyd-500035</p>
+                </div>
+                <label className="icon_label">Burp-Guaranteed</label>
+            </div>
+            <div className='active_hours'>
+                <label>Hours</label>
+                <div>
+                    <p>Monday-Friday <p className='timings'>9am to 10:30pm</p> Saturday-Sunday <p className='timings'>7am to 2am</p></p>
+                </div>
+            </div>
+            <div>
+                <label>Contact</label>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
