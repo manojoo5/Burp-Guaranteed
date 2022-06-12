191220c5a44e0dc7dee9ee9f0dacbb077f91b3f4
Manoj Roy <manojkumarv085@gmail.com>
2022-06-12 15:40:42 +0530

    second push

--- src/Layouts/NavBar/Navbar.js
+++ src/Layouts/NavBar/Navbar.js
@@ -3,10 +3,10 @@ import "./Navbar.css"
 function Navbar(){
     return(
         <div className="navbar">
-            <div>Menu</div>
-            <div className="logo_name">Burp-Guaranteed.</div>
-            <div className="user-action-btn">
-                <button className="pr-2">
+            <div className="mw-25">Menu</div>
+            <div className="logo_name mw-25 text_center">Burp-Guaranteed.</div>
+            <div className="user-action-btn mw-25 text_right">
+                <button className="signup-btn">
                     Sign Up
                 </button>
                 <button>
