aacae23b8ccd8d4dad861986b055f9126d33d52d
Pranitha Bandari <bandaripranitha14@gmail.com>
2022-06-12 20:31:16 +0530

    navigation

--- src/Layouts/Footer/Footer.js
+++ src/Layouts/Footer/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
                 <label>Location</label>
                 <div className='location_text_icon'>
                     <img src={Location} alt="loacation" />
-                    <p>Road No:12,Padma Nagar,Banjara Hills,Hyd-500035</p>
+                   <a href="https://www.google.co.in/maps/place/Road+No.+5,+Devera+Konda+Basti,+Green+Valley,+Banjara+Hills,+Hyderabad,+Telangana+500034/@17.4240855,78.440173,17z/data=!3m1!4b1!4m5!3m4!1s0x3bcb9732aa33efd1:0xd61f7aa1b60e4149!8m2!3d17.4240804!4d78.4423617" target="_blank"> <p className='address'>Road No. 5,Devera Konda Basti, Green Valley, Banjara Hills,Hyderabad, Telangana 500034</p></a>
                 </div>
                 <label className="icon_label">Burp-Guaranteed</label>
             </div>
@@ -22,8 +22,10 @@ function Footer() {
             </div>
             <div>
                 <label>Contact</label>
-                <div>
-                    +9197**78**60
+                <div >
+                    <p className='cinfo cinfo1'>Pranitha(+91-7285944887)</p>
+                    <p className='cinfo'>Akshara(+91-9491345046)</p>
+                    <p className='cinfo'>Sathvik(+91-9866659465)</p>
                 </div>
             </div>
         </div>
